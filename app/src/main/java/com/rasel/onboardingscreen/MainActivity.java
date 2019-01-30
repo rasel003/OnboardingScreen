102a89ae681caab6a297a7824ee5da2ef8469718
rasel00x <rasel.00x@gmail.com>
2019-01-30 11:57:44 +0600

    page switch indicator is added to the project

--- app/src/main/java/com/rasel/onboardingscreen/MainActivity.java
+++ app/src/main/java/com/rasel/onboardingscreen/MainActivity.java
@@ -1,13 +1,15 @@
 package com.rasel.onboardingscreen;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    ViewPager viewPager;
-    SlidePagerAdapter slidePagerAdapter;
+    private ViewPager viewPager;
+    private SlidePagerAdapter slidePagerAdapter;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.slideViewPager);
+        mTabLayout = findViewById(R.id.tab_layout);
+
         slidePagerAdapter = new SlidePagerAdapter(this);
         viewPager.setAdapter(slidePagerAdapter);
+
+        mTabLayout.setupWithViewPager(viewPager, true);
     }
 }
